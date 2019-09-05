a84b8060f8e8e5c968c761f420976ce7c2633834
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-09-05 22:15:28 +0300

    Change meta file names to fit jshotload, add ID, format code

--- cards/screen/CutsceneUI.js
+++ cards/screen/CutsceneUI.js
@@ -1,29 +1,31 @@
 export function onDrawCutscene(arg) {
-  delete card.cutscene;
-  card.cutscene = {
-    height: arg.height,
-    startTime: arg.startTime,
-    duration: arg.duration,
-    reverse: arg.reverse,
-    opacity: arg.opacity,
-  }
+    delete card.cutscene;
+    card.cutscene = {
+        height: arg.height,
+        startTime: arg.startTime,
+        duration: arg.duration,
+        reverse: arg.reverse,
+        opacity: arg.opacity,
+    }
 }
 
 export function onDrawScreen() {
-  const player = getControllingPlayer();
-  if (player && player !== getLocalPlayer()) {
-    return;
-  }
-  if(card.cutsceneStarted) {
-    card.cutscene.fraction = min(1,(getTime()-card.cutscene.startTime)/card.cutscene.duration);
-    if(card.cutscene.reverse) {
-      if(card.cutscene.fraction >= 1) { card.cutsceneStarted = false; }
-      card.cutscene.fraction = 1+-1*min(1,(getTime()-card.cutscene.startTime)/card.cutscene.duration);
+    const player = getControllingPlayer();
+    if (player && player !== getLocalPlayer()) {
+        return;
+    }
+    if (card.cutsceneStarted) {
+        card.cutscene.fraction = min(1, (getTime() - card.cutscene.startTime) / card.cutscene.duration);
+        if (card.cutscene.reverse) {
+            if (card.cutscene.fraction >= 1) {
+                card.cutsceneStarted = false;
+            }
+            card.cutscene.fraction = 1 + -1 * min(1, (getTime() - card.cutscene.startTime) / card.cutscene.duration);
 
- }
-    card.height = card.cutscene.fraction*card.cutscene.height;
-    card.opacity = min(1,0.5*card.cutscene.opacity+0.5*card.cutscene.fraction*card.cutscene.opacity);
-    uiRect(0,0,1600, card.height, UiColor.BLACK,{ opacity: card.opacity });
-    uiRect(0, uiGetScreenHeight()-card.height,1600,card.height, UiColor.BLACK, { opacity: card.opacity } );
-  }    
+        }
+        card.height = card.cutscene.fraction * card.cutscene.height;
+        card.opacity = min(1, 0.5 * card.cutscene.opacity + 0.5 * card.cutscene.fraction * card.cutscene.opacity);
+        uiRect(0, 0, 1600, card.height, UiColor.BLACK, {opacity: card.opacity});
+        uiRect(0, uiGetScreenHeight() - card.height, 1600, card.height, UiColor.BLACK, {opacity: card.opacity});
+    }
 }
--- cards/screen/ShowHideCutscene.js
+++ cards/screen/ShowHideCutscene.js
@@ -1,18 +1,24 @@
 export const PROPS = [
-  propCardTargetActor("Target", {
-    label: "Send to:"
-  }),
-  propDecimal("Height", 220),
-  propDecimal("Opacity", 0.9),
-  propDecimal("Duration", 2),
-  propDecimal("Delay", 0),
-  propBoolean("Hide")
+    propCardTargetActor("Target", {
+        label: "Send to:"
+    }),
+    propDecimal("Height", 220),
+    propDecimal("Opacity", 0.9),
+    propDecimal("Duration", 2),
+    propDecimal("Delay", 0),
+    propBoolean("Hide")
 ];
 
 export function onAction(actionMessage) {
-  const target = getCardTargetActor("Target", actionMessage);
-  if (!target) {
-    return;
-  }
-  sendDelayed( props.Delay, target, "DrawCutscene", { height: props.Height, opacity: props.Opacity, duration: props.Duration, reverse: props.Hide, startTime: getTime() });
+    const target = getCardTargetActor("Target", actionMessage);
+    if (!target) {
+        return;
+    }
+    sendDelayed(props.Delay, target, "DrawCutscene", {
+        height: props.Height,
+        opacity: props.Opacity,
+        duration: props.Duration,
+        reverse: props.Hide,
+        startTime: getTime()
+    });
 }
