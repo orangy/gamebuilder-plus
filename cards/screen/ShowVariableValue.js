e1a6cc7ce8b4db328da5d5e67f453bc4b07c0d48
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-09-07 13:28:01 +0300

    Add card description, change property to propColor for better UX

--- cards/screen/ShowVariableValue.js
+++ cards/screen/ShowVariableValue.js
@@ -1,18 +1,27 @@
 export const PROPS = [
-    propCardTargetActor("Target", {
-        label: "Whose variable?"
-    }),
     propNumber('X', 100),
     propNumber('Y', 100),
-    propString('ColorHex', '#ffff00'),
+    propColor('Color', '#ffff00'),
+    propCardTargetActor("Target", {
+        label: "Whose variables?"
+    }),
     propString('Text', 'Gold: #vargold'),
 ];
 
 export function onDrawScreen() {
     card.text = props.Text.replace(/#var([a-zA-Z]*)/g, varReplace);
-    uiText(props.X, props.Y, card.text, colorFromHex(props.ColorHex) || UiColor.WHITE);
+    uiText(props.X, props.Y, card.text, props.Color || UiColor.WHITE);
 }
 
 function varReplace(match, variable) {
     return getVar(variable, getCardTargetActor("Target")) || 0;
+}
+
+export function getCardStatus() {
+    return {
+        description: `Displays text '<color=orange>${props.Text}</color>' with variables from <color=green>${getCardTargetActorDescription("Target")}</color>`,
+        debugText: `Expanded Text:
+
+${props.Text.replace(/#var([a-zA-Z]*)/g, varReplace)}`
+    }
 }
