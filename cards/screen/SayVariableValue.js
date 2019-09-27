b7ab2b3e6ac2befbed9e097b7c01260bbadafa8e
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-09-27 17:31:25 +0300

    Cleanup props and reduce memory usage for Say Variable

--- cards/screen/SayVariableValue.js
+++ cards/screen/SayVariableValue.js
@@ -2,30 +2,34 @@ export const PROPS = [
     propCardTargetActor("Target", {
         label: "Whose variable?"
     }),
-    propString("Message", "I have ${gold} gold."),
-    propDecimal("OffsetAbove", 0),
-    propNumber("TextSize", 1),
-    propDecimal("HideDelay", 0.5, {label: "Hide Delay\nContinuous mode: 0.5"})
+    propString("Message", "I have ${gold} gold.", {}),
+    propDecimal("OffsetAbove", 0, {
+        label: "Offset Above"
+    }),
+    propNumber("TextSize", 1, {
+        label: "Text Size"
+    }),
+    propDecimal("HideDelay", 0.5, {
+        label: "Hide Delay\nContinuous mode: 0.5"
+    })
 ];
 
-const HIDE_DELAY_SECONDS = 0.5;
 const INITIAL_OFFSET_ABOVE = 1;
 const INITIAL_SCALE = 6;
 
 export function onAction(actionMessage) {
-    // We change the variable
-    card.target = getCardTargetActor("Target", actionMessage);
-    const str = expandVariable(props.Message, "Target");
+    const text = expandVariable(props.Message, "Target", actionMessage);
+
     // Create popup text if we don't have it yet.
     if (!card.popupTextActor || !exists(card.popupTextActor)) {
-        card.popupTextActor = clone("builtin:PopupText",
-            getPointAbove(getBoundsSize().y + INITIAL_OFFSET_ABOVE + props.OffsetAbove));
-        send(card.popupTextActor, "SetText", {text: str});
+        let position = getPointAbove(getBoundsSize().y + INITIAL_OFFSET_ABOVE + props.OffsetAbove);
+        card.popupTextActor = clone("builtin:PopupText", position);
+        send(card.popupTextActor, "SetText", {text: text});
         const scale = Math.min(Math.max(INITIAL_SCALE + (props.TextSize || 0) * 0.5, 1), 12);
-        send(card.popupTextActor, "SetScale", scale);
-        send(card.popupTextActor, "SetParent", {parent: myself()});
+        setScaleUniformPlease(card.popupTextActor, scale);
+        attachToParentPlease(card.popupTextActor, myself());
     }
-    card.popupHideTime = getTime() + HIDE_DELAY_SECONDS;
+    sendDelayed(props.HideDelay, myself(), "HideText");
 }
 
 export function onResetGame() {
@@ -34,16 +38,23 @@ export function onResetGame() {
     delete card.popupHideTime;
 }
 
-export function onTick() {
-    if (card.popupHideTime && getTime() > card.popupHideTime) {
-        // Hide the popup.
-        send(card.popupTextActor, "Destroy");
+export function onHideText() {
+    if (card.popupTextActor) {
+        destroySelfPlease(card.popupTextActor);
         delete card.popupTextActor;
-        delete card.popupHideTime;
     }
 }
 
-function expandVariable(text, targetActor) {
-    const actor = targetActor ? getCardTargetActor(targetActor) : myself();
+export function getCardStatus() {
+    return {
+        description: `Displays text popup "<color=orange>${props.Message}</color>" with variables from <color=green>${getCardTargetActorDescription("Target")}</color>`,
+        debugText: `Expanded Text:
+
+${expandVariable(props.Message, "Target")}`
+    }
+}
+
+function expandVariable(text, targetActor, event) {
+    const actor = targetActor ? getCardTargetActor(targetActor, event) : myself();
     return text.replace(/\${([a-zA-Z]*)}/g, (match, variable) => getVar(variable, actor) || 0)
 }
